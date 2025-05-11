50e5e72a160f43f9959aa5ab27758c4e359df0a6
NIHKOOL <wattikorn.nongnamkhao@gmail.com>
2025-05-11 22:31:22 +0700

    Restyle Arrow

--- src/entities/projectiles/Arrow.java
+++ src/entities/projectiles/Arrow.java
@@ -9,47 +9,61 @@ import interfaces.Updatable;
 import interfaces.Renderable;
 
 public class Arrow implements Updatable, Renderable {
-    protected double x, y;
-    protected double speed;
-    protected boolean facingRight;
-    protected boolean active = true;
-    protected Image arrowImage;
+	protected double x, y;
+	protected double speed;
+	protected boolean facingRight;
+	protected boolean active;
+	protected Image arrowImage;
 
-    public Arrow(double x, double y, boolean facingRight) {
-        this.x = x;
-        this.y = y;
-        this.facingRight = facingRight;
-        this.speed = 8;
-        this.arrowImage = Assets.loadImage("samuraiArcher/Arrow.png");
-    }
+	public Arrow(double x, double y, boolean facingRight) {
+		this.x = x;
+		this.y = y;
+		this.facingRight = facingRight;
+		this.speed = 8;
+		this.arrowImage = Assets.loadImage("samuraiArcher/Arrow.png");
+		this.active = true;
+	}
 
-    @Override
-    public void update() {
-        x += facingRight ? speed : -speed;
-    }
+	@Override
+	public void update() {
+		x += facingRight ? speed : -speed;
+	}
 
-    @Override
-    public void render(GraphicsContext gc, Camera camera) {
-        double drawX = x - camera.getX();
-        double drawY = y - camera.getY() + 22;
-        double width = arrowImage.getWidth();
-        double height = arrowImage.getHeight();
+	@Override
+	public void render(GraphicsContext gc, Camera camera) {
+		double drawX = x - camera.getX();
+		double drawY = y - camera.getY() + 22;
+		double width = arrowImage.getWidth();
+		double height = arrowImage.getHeight();
 
-        if (facingRight) {
-            gc.drawImage(arrowImage, drawX, drawY);
-        } else {
-            gc.drawImage(arrowImage, 0, 0, width, height,
-                         drawX + width, drawY, -width, height);
-        }
-    }
+		if (facingRight) {
+			gc.drawImage(arrowImage, drawX, drawY);
+		} else {
+			gc.drawImage(arrowImage, 0, 0, width, height, drawX + width, drawY, -width, height);
+		}
+	}
 
-    public Rectangle2D getHitbox() {
-        return new Rectangle2D(x, y, arrowImage.getWidth(), arrowImage.getHeight());
-    }
+	public Rectangle2D getHitbox() {
+		return new Rectangle2D(x, y, arrowImage.getWidth(), arrowImage.getHeight());
+	}
 
-    public boolean isActive() { return active; }
-    public void deactive() { active = false; }
-    public double getX() { return x;}
-    public double getY() { return y;}
-    public int getDamage() { return 10;}
-} 
+	public boolean isActive() {
+		return active;
+	}
+
+	public void deactive() {
+		active = false;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public int getDamage() {
+		return 10;
+	}
+}
