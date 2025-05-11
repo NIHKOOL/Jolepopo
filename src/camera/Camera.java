14339c5546bb9b31d148866cd8b1f9de718c6fab
NIHKOOL <wattikorn.nongnamkhao@gmail.com>
2025-05-11 20:28:08 +0700

    Restyle for Camera

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -4,23 +4,29 @@ import config.GameConfig;
 import entities.Character;
 
 public class Camera {
-    private double x, y;
-    private final double screenWidth;
+	private double x, y;
+	private final double screenWidth;
 
-    public Camera(double screenWidth, double screenHeight) {
-        this.screenWidth = screenWidth;
-    }
+	public Camera(double screenWidth, double screenHeight) {
+		this.screenWidth = screenWidth;
+	}
 
-    public void update(Character player, boolean isScrollable) {
-        if (isScrollable) {
-        	x = Math.max(0, Math.min(player.getX() - screenWidth / 2.0, GameConfig.MAP_WIDTH - screenWidth));
-        	if (x < 0) x = 0;
-        } else {
-        	x = 0;
-        }     
-        y = 0;
-    }
+	public void update(Character player, boolean isScrollable) {
+		if (isScrollable) {
+			x = Math.max(0, Math.min(player.getX() - screenWidth / 2.0, GameConfig.MAP_WIDTH - screenWidth));
+			if (x < 0)
+				x = 0;
+		} else {
+			x = 0;
+		}
+		y = 0;
+	}
 
-    public double getX() { return x;}
-    public double getY() { return y;}
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
 }
